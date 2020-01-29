cbdc67ba0a327aca2f1fdb92ff02866e8ff63157
veketable <veikka.mikkola@gmail.com>
2020-01-29 17:40:10 +0200

    ex3

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -20,3 +20,18 @@ function loadDoc(page) {
     xhttp.open("GET", "page1.html", true);
     xhttp.send();
   }
+
+  function showLanguage(){
+	  var select=document.getElementById("source_language");
+	  console.log(select);
+	  var source_language=select.value;
+	  console.log(source_language);
+	  var target_language=document.getElementById("target_language");
+	  target_language.value=source_language;
+	}
+
+	function showPhone(phone){
+	  console.log(phone);
+	  var target_phone=document.getElementById("target_phone");
+	  target_phone.value=phone;
+	}
